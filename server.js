13375b35841fc36bf1c4009ae49cfb7f417f39c2
TheFutureGuide <omkm1157@gmail.com>
2025-11-05 00:32:37 +0530

    Fix test-db route and MySQL config

--- server.js
+++ server.js
@@ -158,6 +158,6 @@ function sendBulkEmails(recipients) {
 }
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port 3000`);
+  console.log(`✅ Server running on port ${port}`);
 });
 
